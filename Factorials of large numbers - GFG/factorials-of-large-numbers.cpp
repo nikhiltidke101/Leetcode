bcb02684758028f98fdf528304b7dfd6e69e4425
Nikhil Tidke <54956597+nikhiltidke101@users.noreply.github.com>
2022-12-14 06:50:25 +0530

    Added solution - LeetHub

---
+++ Factorials of large numbers - GFG/factorials-of-large-numbers.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+public:
+    vector<int> factorial(int N){
+        // code here
+        vector<int> ans;
+        ans.push_back(1);
+        int value =0;
+        int carry =0;
+        
+        for(int i=2; i<=N; i++){
+            for(int j=0; j<ans.size(); j++){
+                value= ans[j]*i+carry;
+                ans[j] = value%10;
+                carry= value/10;
+            }
+            
+            while(carry){
+                value = carry%10;
+                ans.push_back(value);
+                carry /= 10;
+            }
+        }
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        vector<int> result = ob.factorial(N);
+        for (int i = 0; i < result.size(); ++i){
+            cout<< result[i];
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
